86ca6a421e2c5cf17f5766d1d38358732ed523b8
Michael Chow <machow@princeton.edu>
2013-05-13 11:42:05 -0400

    Can calculate ISC against a single  'supersubject'

--- pieman_intersubj.py
+++ pieman_intersubj.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy import io as sio
 from scipy.stats.stats import nanmean
 import nibabel as nib
-from funcs_correlate import crosscor, intersubcorr, load_nii_or_npy, standardize
+from funcs_correlate import crosscor, intersubcorr, load_nii_or_npy, standardize, corsubs
 from pietools import loadwith
 import yaml
 
@@ -19,10 +19,18 @@ cond = cnfg['cond'][condname]
 
 #INPUTS
 subdir = cond['subdir']
-nii_synced = cond['filedir'] + cond['nii_synced']       #synced nifti for each sub
-roi_files = cnfg['roi']                                 #rois for xcorrs
+nii_synced = cond['filedir'] + cond['nii_synced']           #synced nifti for each sub
+if cond['calc_ind']:
+    ind_tc = load_nii_or_npy(cond['ind_tc'])         	    #independent timecourse if specified
+    is_ind = '_ind'
+else:
+    ind_tc = False
+    is_ind = ''
+
+roi_files = cnfg['roi']                                     #rois for xcorrs
 #OUTPUTS
-roi_out = 'intersubj/isc_roi_%s.csv'%condname
+roi_out = 'intersubj/isc_roi_%s%s.csv'%(condname, is_ind)
+roi_mat_out = 'intersubj/isc_roi_mat_%s%s'%(condname, is_ind)
 ccr_out = 'intersubj/ccr_'+condname
 mean_c_out = 'intersubj/ccr_mean_'+condname+'.nii'
 
@@ -42,9 +50,14 @@ ROI_corr = {}
 ROI_corrmat = {}
 for key, roi in roi_maps.items():
     roi_indx = np.nonzero(roi)                                                      #indices where roi is nonzero
-    ccr_roi = crosscor([nanmean(subentry[roi_indx], axis=0) for subentry in dlist], standardized=False)     #crosscors from single ROI timecourse
-    ROI_corr[key] = intersubcorr(ccr_roi)
-    ROI_corrmat[key] = ccr_roi
+    roi_tcs = [nanmean(subentry[roi_indx], axis=0) for subentry in dlist]
+    if np.any(ind_tc):
+        roi_ind_tc = nanmean(ind_tc[roi_indx], axis=0)
+        ROI_corr[key] = np.array([corsubs(sub_tc, roi_ind_tc, standardized=False) for sub_tc in roi_tcs])
+    else:
+        ccr_roi = crosscor(roi_tcs, standardized=False)     #crosscors from single ROI timecourse
+        ROI_corr[key] = intersubcorr(ccr_roi)
+        ROI_corrmat[key] = ccr_roi
 
 #SAVE ROIs
 ROI_corr['Folder'] = sub_indx
@@ -55,10 +68,16 @@ with open(roi_out, 'w') as outfile:
     np.savetxt(outfile, hdr, delimiter=',', fmt='%s')
     np.savetxt(outfile, body, delimiter=',', fmt='%s')
 
+#SAVE ROI CORR MATS
+for key, roi in ROI_corrmat.items():
+    with open('%s_%s.csv'%(roi_mat_out, key), 'w') as outfile:
+        np.savetxt(outfile, np.vstack(sub_indx).T, delimiter=',', fmt='%s')
+        np.savetxt(outfile, np.append(np.vstack(sub_indx), roi, axis=1), delimiter=',', fmt='%s')
+
 #WHOLE BRAIN SUBxSUB CORRELATION MATRIX
 C = crosscor(dlist, standardized = True)
 for ii in range(len(dlist)):
-    C[below_thresh[ii], ii, :] = np.NaN
+    C[below_thresh[ii], ii, :] = np.NaN                 #make entries that didn't meet threshold NA
     C[below_thresh[ii], :, ii] = np.NaN
 
 #Intersubject Correlations
