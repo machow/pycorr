f6b7b57b0ce5f9780ca7259a7502d250e4333f19
Michael Chow <machow@princeton.edu>
2014-02-27 14:13:04 -0500

    made test pipe easy to set up via setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os, shutil, sys
+
+basedir = os.path.dirname(os.path.realpath(__file__))
+shutil.copy2(os.path.join(basedir, 'config.yaml'), 'config.yaml')
+
+
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    shutil.copytree(os.path.join(basedir, 'tests/data/fullpipe'), 'pipeline')
+    shutil.copy2(os.path.join(basedir, 'tests/test_subject.py'), 'run.py')
+    os.symlink('../pieman', 'pieman')
--- tests/test_subject.py
+++ tests/test_subject.py
@@ -4,42 +4,44 @@ if os.path.isfile('test.h5'): os.remove('test.h5')
 import h5py, yaml
 import numpy as np
 from pieman.funcs_correlate import lagcor
-import pieman.workflows
-reload(pieman.workflows)
 from pieman.workflows import isc_within, isc_between
 import pieman.subject as subject
-reload(pieman.subject)
+
+# Setup ---------------------------------------------------
 
 f = h5py.File('test.h5')
 E = subject.Exp(f)
 
 config = yaml.load(open('config.yaml'))
 E.setup(config, create_conds=True)
+
+# Iterate over conditions ---------------------------------
+
 for condname in config['conds']:
     cond = E.get_cond(condname)
     for run in E.iter_runs(condname):
         run.threshold(cond.attrs['threshold'], save=True)
 
-
-    #Alignment
-    a1 = E.f['rois/audenv_3mm_gdboth_wordscram_mean_thr0.2'][...]
+    # Alignment -------------------------------------------
+    a1 = E.f['rois/audenv_3mm_gdboth_wordscram_mean_thr0.2'][...]   #TODO remove hardcoded roi?
     aud_env = cond['audio_env']
-
-    lags = xrange(-15,16)
+    # Calculate lags over range, then set offset
+    lags = xrange(-5,5)
     for run in E.iter_runs(condname):
         data = run.load(roi=a1, standardized=True, threshold=True)#slice(None,None,None))
         corrs = [lagcor(data, aud_env, h=h) for h in lags]
         print corrs
         offset = lags[np.argmax(corrs)]
         run.attrs['offset'] = offset
-    
+
+    # With offsets down, can generate composite
     E.gen_composite(condname)
 
-#SECOND RUN for testing
-cond1 = E.get_cond('wordscram')
+# ISC Calculations ----------------------------------------
+
+cond1 = E.get_cond('wordscram')     # conditions for calculations
 cond2 = E.get_cond('wordscram2')
 
-
-#test isc
+# Run calculations
 isc_within(E, cond1)
 isc_between(E, cond1, cond2, method='inter-subject')
