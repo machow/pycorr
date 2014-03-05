e291d7a0baa0b5415ae3b12d935e9cfeb3dc4a07
Michael Chow <machow@princeton.edu>
2014-03-05 14:44:32 -0500

    wrap copying data in try/except

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ shutil.copy2(os.path.join(basedir, 'config.yaml'), 'config.yaml')
 
 
 if len(sys.argv) > 1 and sys.argv[1] == 'test':
-    shutil.copytree(os.path.join(basedir, 'tests/data/fullpipe'), 'pipeline')
+    try:
+        shutil.copytree(os.path.join(basedir, 'tests/data/fullpipe'), 'pipeline')
+    except: print "can't find test data..."
     shutil.copy2(os.path.join(basedir, 'tests/test_subject.py'), 'run.py')
     os.symlink(os.path.abspath('../pieman'), 'pieman')
