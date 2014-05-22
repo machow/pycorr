9abc1c427e6b6cbd50fe7bfc43a08ecb08f29e64
Michael Chow <machow@princeton.edu>
2014-05-22 16:46:16 -0400

    added test for splice_dir order. Context manager to chdirs

---
+++ tests/helper.py
@@ -0,0 +1,15 @@
+import os, contextlib
+ 
+@contextlib.contextmanager
+def temp_chdir(path):
+    """
+    Usage:
+    >>> with temp_chdir(gitrepo_path):
+    ...   subprocess.call('git status')
+    """
+    starting_directory = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(starting_directory)
---
+++ tests/test_pietools.py
@@ -0,0 +1,8 @@
+#import tempfile, os
+#from helper import temp_chdir 
+from pieman.pietools import sort_int_folder
+
+def test_sort_int_folder():
+    files = ['1.npy', '10.npy', '2.npy']
+    sort_int_folder(files)
+    assert files == ['1.npy', '2.npy', '10.npy']
