179fec50d4048eab3c0a08a2498e4bcba5768f67
Michael Chow <machow@princeton.edu>
2014-06-25 11:58:04 -0400

    added setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pycorr',
+      version='0.1',
+      description='inter-subject correlation analyses library',
+      url='http://github.com/machow/pycorr',
+      author='Michael Chow',
+      author_email='machow@princeton.edu',
+      packages=['pycorr'],
+      zip_safe=False)
